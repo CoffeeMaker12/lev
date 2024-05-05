d6231a3539790928408fd1554b6c484d71171310
CoffeeMaker12 <67kota2001@gmail.com>
2024-05-05 16:02:31 +0900

    8-4途中

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,12 +10,17 @@ class PostController extends Controller
 	public function index(Post $post)
 	{
 	    return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
-	    //getPaginateByLimit()はPost.phpで定義したメソッドです。
+	    //getPaginateByLimit()はPost.phpで定義したメソッド
 	} 
 	
 	public function show(Post $post)
 	{
 	    return view('posts.show')->with(['post' => $post]);
-	 //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
+	 //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス
+	}
+	
+	public function create()
+	{
+	    return view('posts.create');
 	}
 }
